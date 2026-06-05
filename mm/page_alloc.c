962d206b70f14f79036b7d74d423b4af3e50efc8
Rimaki <rimaki5891@gmail.com>
2026-06-05 22:40:45 +0700

    mm: page_alloc: bump watermark scale and set extra free kbytes
    tweak the watermarks further since we are going for a safer background
    reclaim layout. push watermark_scale_factor up to 200 (2.0%).
    
    instead of disabling extra_free_kbytes entirely like some other custom setups
    do, bump it to 32768 (32MB). this lifts the high watermark dynamically,
    giving kswapd a much earlier heads-up to start pushing low-priority pages
    into zram before the system trips into direct reclaim lag during heavy gaming.
    
    Change-Id: Id5f8352a230855a9ce8097fda564c9bf5d35eefb

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -339,14 +339,14 @@ int watermark_boost_factor __read_mostly;
 #else
 int watermark_boost_factor __read_mostly = 0;
 #endif
-int watermark_scale_factor = 150;
+int watermark_scale_factor = 200;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
  * free memory, to make space for new workloads. Anyone can allocate
  * down to the min watermarks controlled by min_free_kbytes above.
  */
-int extra_free_kbytes = 0;
+int extra_free_kbytes = 32768;
 
 static unsigned long nr_kernel_pages __initdata;
 static unsigned long nr_all_pages __initdata;
