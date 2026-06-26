6f968b081dd36d235ceb4584472d17e0039c3db6
ris <rissu.ntk@gmail.com>
2026-06-26 12:03:15 +0000

    exec: Guard LMKD reloading prop hack
    * We don't need this hack when we're using userspace low memory killer.
    
    Change-Id: I7c24974f32a0930b600d9db72471781e7b6dc290
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1745,6 +1745,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
 static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 {
 	const char __user *str;
@@ -1765,6 +1766,7 @@ static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 
 	return !strcmp(buf, "--reinit");
 }
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1887,6 +1889,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 		bprm->argc = 1;
 	}
 
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
 	// Super nasty hack to disable lmkd reloading props
 	if (unlikely(strcmp(bprm->filename, "/system/bin/lmkd") == 0)) {
 		if (is_lmkd_reinit(&argv)) {
@@ -1895,6 +1898,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 			goto out;
 		}
 	}
+#endif
 
 	retval = exec_binprm(bprm);
 	if (retval < 0)
