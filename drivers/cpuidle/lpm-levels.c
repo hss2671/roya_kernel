dereference23/kernel_xiaomi_sm6375

lpm-levels: Disable deep idle states when the screen is on
Using only the shallowest idle state (WFI) paradoxically improves both
performance and power consumption. However, deep idle states save a significant
amount of energy in very light workloads that are typical when the screen is off.

Change-Id: If289a1ff8abc55bcaee92933905a7a05a01581ad
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

drivers/cpuidle/lpm-levels.c
@@ -42,6 +42,9 @@
 #include "lpm-levels.h"
 #include <trace/events/power.h>
 #include <linux/clk.h>
+#ifdef CONFIG_DRM_PANEL
+#include <drm/drm_panel.h>
+#endif
 #define CREATE_TRACE_POINTS
 #include <trace/events/trace_msm_low_power.h>
 
@@ -93,8 +96,46 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 		const struct cpumask *cpu, int child_idx, bool from_idle,
 		int64_t time);
 
+#ifdef CONFIG_DRM_PANEL
+static bool sleep_disabled = true;
+module_param_named(sleep_disabled, sleep_disabled, bool, 0444);
+
+static int lpm_drm_panel_notify(struct notifier_block *nb,
+		unsigned long val, void *ptr)
+{
+	struct drm_panel_notifier *evdata = ptr;
+	int *blank = evdata->data;
+
+	switch (*blank) {
+	case DRM_PANEL_BLANK_UNBLANK:
+		if (val == DRM_PANEL_EARLY_EVENT_BLANK) {
+			sleep_disabled = true;
+			wake_up_all_idle_cpus();
+		}
+		break;
+	case DRM_PANEL_BLANK_POWERDOWN:
+	case DRM_PANEL_BLANK_LP:
+		if (val == DRM_PANEL_EARLY_EVENT_BLANK) {
+			sleep_disabled = false;
+			wake_up_all_idle_cpus();
+		}
+		break;
+	default:
+		break;
+	};
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block drm_notifier = {
+	.notifier_call = lpm_drm_panel_notify,
+};
+
+extern struct drm_panel *goodix_get_panel(void);
+#else
 static bool sleep_disabled;
 module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
+#endif
 
 #ifdef CONFIG_SMP
 static int lpm_cpu_qos_notify(struct notifier_block *nb,
@@ -1716,6 +1757,18 @@ static int lpm_probe(struct platform_device *pdev)
 	unsigned int cpu;
 	struct hrtimer *cpu_histtimer;
 	struct kobject *module_kobj = NULL;
+#ifdef CONFIG_DRM_PANEL
+	struct drm_panel *active_panel = goodix_get_panel();
+
+	if (!active_panel)
+		return -EPROBE_DEFER;
+
+	ret = drm_panel_notifier_register(active_panel, &drm_notifier);
+	if (ret)
+		pr_err("Failed to register drm panel notifier: %d\n", ret);
+	else
+		pr_info("Registered drm panel notifier\n");
+#endif
 
 	get_online_cpus();
 	lpm_root_node = lpm_of_parse_cluster(pdev);

hss2671/roya_kernel

Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

drivers/cpuidle/lpm-levels.c
@@ -42,6 +42,9 @@
 #include "lpm-levels.h"
 #include <trace/events/power.h>
 #include <linux/clk.h>
+#ifdef CONFIG_DRM_PANEL
+#include <drm/drm_panel.h>
+#endif
 #define CREATE_TRACE_POINTS
 #include <trace/events/trace_msm_low_power.h>
 
@@ -120,8 +123,46 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 		const struct cpumask *cpu, int child_idx, bool from_idle,
 		int64_t time);
 
+#ifdef CONFIG_DRM_PANEL
+static bool sleep_disabled = true;
+module_param_named(sleep_disabled, sleep_disabled, bool, 0444);
+
+static int lpm_drm_panel_notify(struct notifier_block *nb,
+		unsigned long val, void *ptr)
+{
+	struct drm_panel_notifier *evdata = ptr;
+	int *blank = evdata->data;
+
+	switch (*blank) {
+	case DRM_PANEL_BLANK_UNBLANK:
+		if (val == DRM_PANEL_EARLY_EVENT_BLANK) {
+			sleep_disabled = true;
+			wake_up_all_idle_cpus();
+		}
+		break;
+	case DRM_PANEL_BLANK_POWERDOWN:
+	case DRM_PANEL_BLANK_LP:
+		if (val == DRM_PANEL_EARLY_EVENT_BLANK) {
+			sleep_disabled = false;
+			wake_up_all_idle_cpus();
+		}
+		break;
+	default:
+		break;
+	};
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block drm_notifier = {
+	.notifier_call = lpm_drm_panel_notify,
+};
+
+extern struct drm_panel *fts_get_panel(void);
+#else
 static bool sleep_disabled;
 module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
+#endif
 
 #ifdef CONFIG_SMP
 static int lpm_cpu_qos_notify(struct notifier_block *nb,
@@ -1778,6 +1819,18 @@ static int lpm_probe(struct platform_device *pdev)
 	unsigned int cpu;
 	struct hrtimer *cpu_histtimer;
 	struct kobject *module_kobj = NULL;
+#ifdef CONFIG_DRM_PANEL
+	struct drm_panel *active_panel = fts_get_panel();
+
+	if (!active_panel)
+		return -EPROBE_DEFER;
+
+	ret = drm_panel_notifier_register(active_panel, &drm_notifier);
+	if (ret)
+		pr_err("Failed to register drm panel notifier: %d\n", ret);
+	else
+		pr_info("Registered drm panel notifier\n");
+#endif
 
 	get_online_cpus();
 	lpm_root_node = lpm_of_parse_cluster(pdev);