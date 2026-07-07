dereference23/kernel_xiaomi_sm6375

techpack: display: Disable transition to LP2 for video mode panels
From veux-r-oss

Change-Id: I3255429e5663eaf11182cf823b9fcc1d251a86c8
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

techpack/display/msm/sde/sde_kms.c
@@ -3992,6 +3992,7 @@ retry:
 			continue;
 
 		lp = sde_connector_get_lp(conn);
+#ifndef CONFIG_WT_QGKI
 		if (lp == SDE_MODE_DPMS_LP1) {
 			/* transition LP1->LP2 on pm suspend */
 			ret = sde_connector_set_property_for_commit(conn, state,
@@ -4003,6 +4004,7 @@ retry:
 				goto unlock;
 			}
 		}
+#endif
 
 		if (lp != SDE_MODE_DPMS_LP2) {
 			/* force CRTC to be inactive */

hss2671/roya_kernel

techpack/display/msm/sde/sde_kms.c
@@ -4017,6 +4017,7 @@ retry:
 			continue;
 
 		lp = sde_connector_get_lp(conn);
+#ifndef CONFIG_HQ_QGKI
 		if (lp == SDE_MODE_DPMS_LP1) {
 			/* transition LP1->LP2 on pm suspend */
 			ret = sde_connector_set_property_for_commit(conn, state,
@@ -4028,6 +4029,7 @@ retry:
 				goto unlock;
 			}
 		}
+#endif
 
 		if (lp != SDE_MODE_DPMS_LP2) {
 			/* force CRTC to be inactive */